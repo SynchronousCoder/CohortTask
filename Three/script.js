7e06675a8589cb471219fe5fc6b2d18b5d8d60a7
Aryan Tyagi <dontfollowmann@gmail.com>
2025-11-21 23:06:58 +0530

    Making Responsive

--- Three/script.js
+++ Three/script.js
@@ -1,16 +1,27 @@
+import * as THREE from 'three';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-const canvas = document.querySelector("#main");
+
+const canvas = document.querySelector('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+//responsive
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
 
 function animate() {
     window.requestAnimationFrame(animate);
@@ -18,4 +29,4 @@ function animate() {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 }
-animate();
+animate();  
