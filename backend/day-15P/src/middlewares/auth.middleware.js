a7146e3bf59b00eec1d287c7dfce7a2226ef9c7c
SynchronousCoder <dontfollowmann@gmail.com>
2026-08-14 15:02:53 +0530

    Backend-day17P: Middleware(req object use) + Controller(use of req.user [req obj])

--- backend/day-15P/src/middlewares/auth.middleware.js
+++ backend/day-15P/src/middlewares/auth.middleware.js
@@ -2,23 +2,26 @@ const jwt = require("jsonwebtoken")
 
 async function identifyUser(req, res, next) {
     const token = req.cookies.token
-
     if(!token){
-            res.status(404).json({
-        message: "Unauthrozied user, Token not found"
-    })
+        res.status(404).json({
+            message: "Unauthrozied user, Token not found"
+        })
     }
-
-    let decoded
+    
+    let decoded = null
     try {
         decoded = jwt.verify(token, process.env.JWT_SECRET)
     } catch (error) {
         res.status(409).json({
-            message: "Unauthrozied user, Token not verified"
+            message: "Unauthrozied User, Token not verified"
         })
     }
 
+    // attaching data to the request object (It’s basically a custom property on the req object.)
+    // that property (aryan) becomes part of the request object (req) and is 
+    // available to all subsequent middleware and route handlers in the chain.
     req.user = decoded
+
     next()
 }
 
