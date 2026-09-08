e1a9986fbd65487c888b599d3410e5ecbe9ac43e
SynchronousCoder <dontfollowmann@gmail.com>
2026-09-08 23:21:18 +0530

    Learned basic of socket-io

--- backend/day-30/src/validators/authValidator.js
+++ backend/day-30/src/validators/authValidator.js
@@ -11,6 +11,7 @@ const validate = (req, res, next) => {
     errors: errors.array(),
   });
 };
+
 export const registerValidator = [
   body("email").isEmail().withMessage("Valid email required"),
   body("password")
